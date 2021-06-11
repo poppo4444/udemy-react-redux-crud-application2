6d06eedcf39b1f1c3755f3e043fd8a19b7749211
poppo4444 <aritomo44@icloud.com>
2021-06-11 17:31:59 +0900

    :sparkles: propsの学習

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 function App() {
+  const profiles = [
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako' }
+  ]
   return (
-    <div className="App">
-      <h1>
-        Hello,World
-      </h1>
+    <div>
+      {
+        profiles.map((profile, index) =>{
+          return <User name={profile.name} age={profile.age} key={index}/>
+        })
+      }
     </div>
   );
+}
+
+const User = (props) => {
+  return <div>Hi! i am {props.name}! and age is {props.age}</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
